76a733e65c78623d30229430d39e56f5ac3d9058
Satish Patel <satish.patel@linaro.org>
2017-08-14 13:07:12 +0530

    Add build support using gradlew
    - Add Android.mk - which use gradlew
    - Sign generate apk using plaform key
    
    Signed-off-by: Satish Patel <satish.patel@linaro.org>
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+# Note: Make sure ANDROID_HOME is exported with sdk path
+$(info $(shell ($(LOCAL_PATH)/gradlew assembleRelease -p $(LOCAL_PATH)/)) )
+$(info $(shell ($(LOCAL_PATH)/finalize.sh $(PRODUCT_OUT))))
+
+
