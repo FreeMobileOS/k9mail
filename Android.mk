cc0cb2ac95947d0aa675daa94c247d155ea7e4ea
Satish Patel <tosatishpatel@gmail.com>
2017-09-04 19:31:42 +0530

    Updated mk file to use relative path
    Android.mk and finalize.sh should use relative path
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 # Note: Make sure ANDROID_HOME is exported with sdk path
 $(info $(shell ($(LOCAL_PATH)/gradlew assembleRelease -p $(LOCAL_PATH)/)) )
-$(info $(shell ($(LOCAL_PATH)/finalize.sh $(PRODUCT_OUT))))
+$(info $(shell ($(LOCAL_PATH)/finalize.sh $(PRODUCT_OUT_PATH))))
 
 
--- finalize.sh
+++ finalize.sh
@@ -1,9 +1,14 @@
 BASEDIR=$(dirname $0)
 PRODUCT_PATH=$1
-echo $PRODUCT_PATH
-
+echo Product path:$PRODUCT_PATH
+echo Android path:$ANDROID_SRC_PATH
 # zipalign
 zipalign -v -p 4 $BASEDIR/k9mail/build/outputs/apk/k9mail-release-unsigned.apk $BASEDIR/k9mail/build/outputs/apk/k9mail-release-unsigned-aligned.apk
 
+if [ -z "$ANDROID_SRC_PATH" ] ; then
+    echo "Android source path is not set. Sign apk manually"
+    return 0
+fi
+
 # sign apk with platform key
-java -Xmx1024m -Djava.library.path="$BASEDIR/../../../out/host/linux-x86/lib64" -jar $BASEDIR/../../../out/host/linux-x86/framework/signapk.jar $BASEDIR/../../../build/target/product/security/platform.x509.pem $BASEDIR/../../../build/target/product/security/platform.pk8 $BASEDIR/k9mail/build/outputs/apk/k9mail-release-unsigned-aligned.apk $PRODUCT_PATH/system/app/k9mail.apk
+java -Xmx1024m -Djava.library.path="$ANDROID_SRC_PATH/out/host/linux-x86/lib64" -jar $ANDROID_SRC_PATH/out/host/linux-x86/framework/signapk.jar $ANDROID_SRC_PATH/build/target/product/security/platform.x509.pem $ANDROID_SRC_PATH/build/target/product/security/platform.pk8 $BASEDIR/k9mail/build/outputs/apk/k9mail-release-unsigned-aligned.apk $PRODUCT_PATH/k9mail.apk
